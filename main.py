76e3d640b8686678d5eeb4cab6eaf50a0be0c83e
Lee Jae Jun <56428918+Linho1150@users.noreply.github.com>
2019-11-22 14:10:26 +0900

    ACCEESS_TO_MYSQL(RDS)

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import asyncio
 import aiohttp
 import json
 import pypyodbc
+import time
+import pymysql
 
 def buslist_file_load():
     f = open("buslist.txt", 'rt',encoding='UTF8')
@@ -24,7 +26,7 @@ async def busdetail(busRouteId):
         async with aiohttp.ClientSession() as session:
             async with session.get('http://m.bus.go.kr/mBus/bus/getRouteAndPos.bms?busRouteId='+busRouteId) as resp:
                 jsondata=json.loads(await resp.text())
-                TEMP=[(i['stationNm'],i['existYn']) for i in jsondata['resultList']] #stationNo-정류소번호 #stationNm=정류소이름
+                TEMP=[(i['stationNo'],i['existYn']) for i in jsondata['resultList']] #stationNo-정류소번호 #stationNm=정류소이름
                 return TEMP
                 # existYn 정류소에 버스 여부
     except:
@@ -33,31 +35,41 @@ async def busdetail(busRouteId):
 
 async def main(totalbuslist):
         try:
+            Tbusnumber = ""
+            Tstationnm = ""
+            Texist = ""
             for busnumber in totalbuslist:
                 busnum=[busdetail(await busnumberToid(busnumber))]
                 result=await asyncio.gather(*busnum)
                 for bus in result:
                     for stationnm, exist in bus:
-                        access_insert_data(busnumber,stationnm.strip(),exist.strip())
-        except:
-            print(busnumber,"정보를 DATABASE에 데이터를 저장하지 못하였습니다.")
+                        if exist=="Y" and Texist != exist:
+                            mysql_insert_data(busnumber, stationnm.strip(), exist.strip())
+                            Texist = exist
+                            print(busnumber,",",stationnm.strip(),"의 변동이 확인되었습니다.")
+
+
+        except Exception as ex:  # 에러 종류:
+            print(busnumber,"정보를 DATABASE에 데이터를 저장하지 못하였습니다.\n",ex)
             pass
 
-def access_insert_data(busnumber,stationnumber,exist):
-    cur = dbconn.cursor()
-    cur.execute("""INSERT INTO BUS_LIVE VALUES('"""+busnumber+"""','"""+stationnumber+"""',now(),'"""+exist+"""')""")
-    dbconn.commit()
-
-#DB초기화 ----------------------------------------
-dbname = r'C:\Users\LINHO\BIG_DATA_PROJECT\DATA.mdb'
-dir = r'C:\Users\LINHO\BIG_DATA_PROJECT'
-
-connStr = (r"DRIVER={Microsoft Access Driver (*.mdb)};UID=admin;UserCommitSync=Yes;Threads=3;SafeTransactions=0;PageTimeout=5;axScanRows=8;MaxBufferSize=2048;FIL={MS Access};DriverId=25;DefaultDir=" + dir + ";DBQ=" + dbname)
-dbconn = pypyodbc.connect(connStr)
-#DB초기화 완료-------------------------------------
+def mysql_insert_data(busnumber,stationnumber,exist):
+    # MySQL초기화시작---------------------------------------
+    conn = pymysql.connect(host='bus-live.canb61sq58tb.ap-northeast-2.rds.amazonaws.com', user='admin',
+                           password='wowns0034', db='bus-live', charset='utf8')
+    # MySQL초기화완료---------------------------------------
+    curs = conn.cursor()
+    sql = """INSERT INTO BUS_LIVE VALUES(%s,%s,%s,%s)"""
+    now = time.localtime()
+    curs.execute(sql, (busnumber, stationnumber, int(str(now.tm_year)+str(now.tm_mon)+str(now.tm_mday)+str(now.tm_hour)+str(now.tm_min)+str(now.tm_sec)), exist))
+    conn.commit()
+    conn.close()
 
 #프로세스 시작--------------------------------------
 totalbuslist=buslist_file_load()
-asyncio.run(main(totalbuslist))
+
+while(True):
+    time.sleep(1)
+    asyncio.run(main(totalbuslist))
 
 
