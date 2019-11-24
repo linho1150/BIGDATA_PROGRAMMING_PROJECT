a92bf682b97ad6835d5057387aed7ed2d61482c9
Lee Jae Jun <56428918+Linho1150@users.noreply.github.com>
2019-11-24 18:54:26 +0900

    LINUX_V2
    change str type to datetime
    cahnge str to BIGINT

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import time
 import pymysql
 import datetime
 
-
 def buslist_file_load():
     f = open("buslist.txt", 'rt',encoding='UTF8')
     line = f.read().splitlines()
@@ -36,6 +35,13 @@ async def busdetail(busRouteId):
         print(busRouteId+"의 데이터를 로드하지 못하였습니다.")
         pass
 
+async def save_log(log,busnumber,ex):
+    now = datetime.datetime.now()
+    now_time = now.strftime("%y%m%d%H%M%S")
+    file1 = open("ERROR_LOG.txt", "w")
+    file1.write("[" + str(now_time) + "]" +log+"\n"+ str(busnumber) + "정보를 DATABASE에 데이터를 저장하지 못하였습니다.\n" + str(ex) + "\n")
+    file1.close()
+
 async def main(totalbuslist,index):
         try:
             for busnumber in totalbuslist:
@@ -45,56 +51,78 @@ async def main(totalbuslist,index):
                     for stationnm, exist in bus:
                         str_exist=str(exist).strip().replace(" ","")
                         str_stationnm=str(stationnm).strip().replace(" ","")
-                        str_before_exist,str_before_time = await mysql_search_data(busnumber, str_stationnm)
+                        str_before_exist = await mysql_search_data(busnumber, str_stationnm)
                         if str_exist != str_before_exist or index==0:
-                            await mysql_insert_data(busnumber, str_stationnm, str_exist,index,str_before_time)
-
+                            await mysql_insert_data(busnumber, str_stationnm, str_exist,index)
+                print("confirm : ",str(index))
         except Exception as ex:
-            try:
-                now = datetime.datetime.now()
-                now_time = now.strftime("%y%m%d%H%M%S")
-                print("[" + str(now_time) + "]" + str(busnumber) + "정보를 DATABASE에 데이터를 저장하지 못하였습니다.\n" + str(ex) + "\n")
-                file1 = open("ERROR_LOG.txt","w")
-                file1.write("["+str(now_time)+"]"+str(busnumber)+"정보를 DATABASE에 데이터를 저장하지 못하였습니다.\n"+str(ex)+"\n")
-                file1.close()
-            except Exception as e:
-                print("ERROR",e)
-                print(busnumber,"정보를 DATABASE에 데이터를 저장하지 못하였습니다.\n",ex)
-                pass
+            await save_log("function main",busnumber,ex)
+            print("main",ex);
+            pass
 
-async def mysql_insert_data(busnumber,stationnumber,exist,index,str_before_time):
+async def mysql_insert_data(busnumber,stationnumber,exist,index):
     try:
-        conn = pymysql.connect(host='bus-live.canb61sq58tb.ap-northeast-2.rds.amazonaws.com', user='admin',
-                               password='wowns0034', db='bus-live', charset='utf8')
+        conn = pymysql.connect(host='bus-live.canb61sq58tb.ap-northeast-2.rds.amazonaws.com', user='admin', password='wowns0034', db='bus-live', charset='utf8')
+
+        #시간시작----------------------------------------------------
+        str_before_time = await mysql_get_time_data(busnumber, stationnumber)
+        try:
+            now = datetime.datetime.now()
+            now_time = now.strftime("%y-%m-%d %H:%M:%S")
+            wtime=str((now-str_before_time).seconds)
+        except:
+            wtime="0"
+            pass
+        # 시간끝----------------------------------------------------
+
+        #MYSQL insert----------------------------------------------
         curs = conn.cursor()
         sql = """INSERT INTO BUS_LIVE VALUES(%s,%s,%s,%s,%s,%s)"""
-        now = datetime.datetime.now()
-        now_time = now.strftime("%y%m%d%H%M%S")
-        wtime=str(int(now_time)-int(str_before_time))
         curs.execute(sql, (index, busnumber, stationnumber, now_time, exist, wtime))
         conn.commit()
-        print("[",now_time,"] ",index," ",busnumber,",",stationnumber,"의 변동이 확인되었습니다.")
+        print(index," ",busnumber,",",stationnumber,"find")
+        conn.close()
+        # MYSQL insert----------------------------------------------
+
+    except Exception as ex:
+        await save_log("function mysql_insert_data",busnumber,ex)
+        print("mysql_insert_data ", ex);
+        pass
+
+async def mysql_get_time_data(busnumber,stationnumber):
+    try:
+        conn = pymysql.connect(host='bus-live.canb61sq58tb.ap-northeast-2.rds.amazonaws.com', user='admin', password='wowns0034', db='bus-live', charset='utf8')
+        curs = conn.cursor()
+        sql = """SELECT B.TIME FROM (SELECT MAX(INDEX_NUM) AS MAX_NUM FROM BUS_LIVE WHERE BUS_ID=%s AND STATION_ID=%s AND BUS_IS='Y') AS A, BUS_LIVE AS B WHERE BUS_ID=%s AND STATION_ID=%s AND B.INDEX_NUM=A.MAX_NUM"""
+        curs.execute(sql, (busnumber,stationnumber,busnumber,stationnumber))
+        result = curs.fetchall()
+        if len(result) == 0:
+            return datetime.datetime.strptime('2019-11-24 00:01:00', "%Y-%m-%d %H:%M:%S")
+        for row_data in result:
+            return row_data[0]
         conn.close()
     except Exception as ex:
-        print("MYSQL 오류",ex)
+        await save_log("function mysql_get_time_data", busnumber, ex)
+        print("mysql_get_time_data ", ex);
         pass
 
 
 async def mysql_search_data(busnumber,stationnumber):
     try:
-        conn = pymysql.connect(host='bus-live.canb61sq58tb.ap-northeast-2.rds.amazonaws.com', user='admin',
-                               password='wowns0034', db='bus-live', charset='utf8')
-        curs = conn.cursor() #WHERE BUS_ID=%s AND STATION_ID=%s AND INDEX_NUM=%s
-        sql = """SELECT B.BUS_IS,B.TIME FROM (SELECT MAX(INDEX_NUM) AS MAX_NUM FROM BUS_LIVE WHERE BUS_ID=%s AND STATION_ID=%s) AS A, BUS_LIVE AS B WHERE BUS_ID=%s AND STATION_ID=%s AND B.INDEX_NUM=A.MAX_NUM"""
+        conn = pymysql.connect(host='bus-live.canb61sq58tb.ap-northeast-2.rds.amazonaws.com', user='admin', password='wowns0034', db='bus-live', charset='utf8')
+        curs = conn.cursor()
+        sql = """SELECT B.BUS_IS FROM (SELECT MAX(INDEX_NUM) AS MAX_NUM FROM BUS_LIVE WHERE BUS_ID=%s AND STATION_ID=%s) AS A, BUS_LIVE AS B WHERE BUS_ID=%s AND STATION_ID=%s AND B.INDEX_NUM=A.MAX_NUM"""
         curs.execute(sql, (busnumber,stationnumber,busnumber,stationnumber))
         result = curs.fetchall()
         if len(result) == 0:
-            return "", 0
+            return "N"
         for row_data in result:
-            return row_data[0],row_data[1]
+            return row_data[0]
         conn.close()
-    except Exception as e:
-        print("MYSQL 오류2",e)
+    except Exception as ex:
+        await save_log("function mysql_search_data", busnumber, ex)
+        print("mysql_search_data ", ex);
+        pass
 
 #프로세스 시작--------------------------------------
 totalbuslist=buslist_file_load()
@@ -105,6 +133,5 @@ while(True):
     loop.run_until_complete(main(totalbuslist,index))
     index=index+1
 loop.close()
-
 
 
